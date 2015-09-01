8f17188f123936bbec5e7ab1d3a457d2215f850c
Qing Liang <liangqing226@gmail.com>
2015-09-01 23:13:35 +0800

    Add deploy script and change the version to 0.0.3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='syncrypto',
-    version='0.0.2',
+    version='0.0.3',
     description='Synchronize a folder with its encrypted content',
     long_description=long_description,
     url='https://github.com/liangqing/syncrypto',
